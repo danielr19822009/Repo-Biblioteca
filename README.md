31786df510d00cec410709472cf9d98daf4892ba
Equipo-Casa <115900087+danielr19822009@users.noreply.github.com>
2024-11-19 05:49:40 -0500

    actualizo archivo server  Proyecto Biblioteca para puiblicar con vercel

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Repo-Biblioteca
+###Proyecto biblioteca publicado en vercel
+
+clonar repositorio 
+
+crear proyecto en vercel conectado a git
+
+    host: 'db-biblioteca.cdwk8g26ozev.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'bdbiblioteca123*',
+    database: 'bd_librarysm',
+
+
+
+
+    
--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'bd_librarysm',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 ////////users///////////////////////////////
@@ -37,6 +37,8 @@ app.post('/add_users', (req, res) => {
         }
     );
 })
+
+
 
 
 
